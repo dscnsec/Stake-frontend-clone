bd70cb3557caeaba04fd31a18a51acd61ba9e20e
Pariharx7 <parihar.peace@gmail.com>
2024-10-25 22:37:14 +0530

    Added Question Component with folder

---
+++ src/components/QuestionComponent/QuestionComponent.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const QuestionComponent = () => {
+  return (
+    <div className="bg-gray-700 w-full h-full">
+        <div className="flex justify-between">
+            <div>
+                <h1>Still have questions?</h1>
+                <button>Read our guides</button>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default QuestionComponent
